4fb23dbb9c06b4eb754aca838114eff16966d81e
EmerickWerbregue <emerick.werbregue41@gmail.com>
2025-01-30 21:53:00 +0100

    Add files via upload

---
+++ Resume emerick.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Merci de m\'avoir contacté !');
+    // Vous pouvez ajouter ici du code pour envoyer le formulaire via un service comme EmailJS
+});
